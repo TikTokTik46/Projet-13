802668aa32cc5e72b340daeb41e28d22fb09e8e2
Quentin Molières <molieres.quentin@gmail.com>
2023-09-07 09:42:20 +0200

    Mise en place de pages d'erreurs personnalisées (404,500,403,400).

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,5 +1,6 @@
 """
-Configuration des URL principales du projet, incluant les applications "lettings" et "profiles".
+Configuration des URL principales du projet, incluant les applications
+"lettings" et "profiles".
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -11,4 +12,11 @@ urlpatterns = [
     path('lettings/', include('lettings.urls', namespace='lettings')),
     path('profiles/', include('profiles.urls', namespace='profiles')),
     path('admin/', admin.site.urls),
+    # Test de la page 404
+    path('404/', oc_lettings_site.views.error_404_view_handler),
 ]
+
+handler404 = 'oc_lettings_site.views.error_404_view_handler'
+handler500 = 'oc_lettings_site.views.error_500_view_handler'
+handler403 = 'oc_lettings_site.views.error_403_view_handler'
+handler400 = 'oc_lettings_site.views.error_400_view_handler'
--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -2,7 +2,7 @@
 Ce fichier contient la vue pour afficher la page d'accueil du site.
 """
 from django.shortcuts import render
-
+from django.utils.translation import gettext_lazy as _
 
 def index(request):
     """
@@ -16,3 +16,47 @@ def index(request):
     """
 
     return render(request, 'index.html')
+
+
+VIEW_ERRORS = {
+    404: {'title': _("404 - Page not found"),
+          'content': _("Il semble que cette page ait disparu dans "
+                       "le cyberespace. Notre équipe de recherche de"
+                       " pages égarées est en route, mais en attendant,"
+                       " vous pouvez retourner à la sécurité de la page"
+                       " d'accueil."), },
+    500: {'title': _("Internal error"),
+          'content': _("Le serveur a peut-être eu un fou rire, mais il essaie "
+                       "de se ressaisir. Revenez plus tard, nous espérons "
+                       "qu'il aura retrouvé son sérieux d'ici là."), },
+    403: {'title': _("Permission denied"),
+          'content': _("Désolé, vous n'avez pas la clé secrète pour cette "
+                       "partie du site. C'est comme essayer d'accéder à la "
+                       "salle des trésors sans être un pirate. "
+                       "Retournez sur la bonne voie."), },
+    400: {'title': _("Bad request"),
+          'content': _("Il semble que votre demande soit tombée dans le "
+                       "mystère des demandes incorrectes."), }, }
+
+def error_view_handler(request, exception, status):
+    """Gère les erreurs HTTP en affichant une page d'erreur personnalisée."""
+    return render(request, template_name='errors.html', status=status,
+                  context={'error': exception, 'status': status,
+                           'title': VIEW_ERRORS[status]['title'],
+                           'content': VIEW_ERRORS[status]['content']})
+
+def error_404_view_handler(request, exception=None):
+    """Gère les erreurs 404 en utilisant la fonction error_view_handler."""
+    return error_view_handler(request, exception, 404)
+
+def error_500_view_handler(request, exception=None):
+    """Gère les erreurs 500 en utilisant la fonction error_view_handler."""
+    return error_view_handler(request, exception, 500)
+
+def error_403_view_handler(request, exception=None):
+    """Gère les erreurs 403 en utilisant la fonction error_view_handler."""
+    return error_view_handler(request, exception, 403)
+
+def error_400_view_handler(request, exception=None):
+    """Gère les erreurs 400 en utilisant la fonction error_view_handler."""
+    return error_view_handler(request, exception, 400)
