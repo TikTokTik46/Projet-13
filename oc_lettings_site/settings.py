ce37dd5fe80fd8b231834fc86d6dcf2f8321bc35
Quentin Molières <molieres.quentin@gmail.com>
2023-10-18 22:06:50 +0200

    Supression du .env de l'image Docker

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -11,6 +11,7 @@ import os
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 from decouple import config
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
